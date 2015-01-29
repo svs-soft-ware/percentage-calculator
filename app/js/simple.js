a06ee64d2eccd4b7771f822d98d6630531b16b40
subin <subin@IN-150>
2015-01-29 12:33:26 +0530

    V1.0.5

--- app/js/simple.js
+++ app/js/simple.js
@@ -25,3 +25,25 @@ var simpleCalc = (function() {
     };
 }());
 
+var fractionCalc = (function() {
+    var res = {'result': null};
+    function calcPercentage(numerator, denominator) {
+        return (numerator / denominator) * 100;
+    }
+    function getResult() {
+        var numerator = $('#numerator').val(),
+                denominator = $('#denominator').val();
+        if (numerator === '' || denominator === '') {
+            return JSON.stringify(res);
+        }
+        res = {'result': calcPercentage(numerator, denominator)};
+        return JSON.stringify(res);
+    }
+    return {
+        validateExecute: function() {
+            var validator = $("#percentage_calculator").validate();
+            validator.form();
+            return getResult();
+        }
+    };
+}());
